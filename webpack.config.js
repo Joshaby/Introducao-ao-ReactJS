706cb060afe22b9678474c050a16d613943b2aca
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2021-10-31 22:43:43 -0300

    Adicionado suporte ao eslint e source map no webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    devtool: 'source-map',
     entry: './src/index.jsx',
     output: {
         path: path.resolve(__dirname, 'dist'),
